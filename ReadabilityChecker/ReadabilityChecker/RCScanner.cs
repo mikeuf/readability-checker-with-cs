e60a8d1ffa6226100f242ed96e41ae2dd564db41
Mike <mblack101@ufl.edu>
2018-11-04 21:21:17 -0500

    converting regex to c# style

---
+++ ReadabilityChecker/ReadabilityChecker/RCScanner.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ReadabilityChecker
+{
+    class RCScanner
+    {
+        // regex patterns that the RCScanner checks for
+        //   private static readonly String PRE_HTML_STRIP_SENTENCE_PATTERN = "((\\w+(,|:)?)(&quot;)?(&#39;)?(<.*>)?(<\\/p>|<\\/h|<\\/li>|<br))";
+
+        public int countIPs(String s)
+        {
+
+            // private static readonly Regex IP_PATTERN = new Regex("((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+           var IP_PATTERN = new Regex(@"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+
+            
+         int i = 0;
+       //  Pattern p = Pattern.compile(IP_PATTERN);
+       //  Matcher matcher = p.matcher(s);
+       var m = IP_PATTERN.Match(s);
+
+             while (matcher.find())
+             {
+                 ++i;
+             }
+             return i;
+                     
+        }
+
+
+        public int countLists(String s)
+{
+
+        private static readonly String LIST_PATTERN = "((<.*>)?(<p>|<h>|<li>|<br>|<br \\/>)(<.*>)?((\\d{1,2}\\.)|(\\d{1,2}\\))\\s))";
+
+int i = 0;
+Pattern p = Pattern.compile(LIST_PATTERN);
+Matcher matcher = p.matcher(s);
+            while (matcher.find())
+            {
+                ++i;
+            }
+            return i;
+        }
+
+
+
+        public int countNBSPs(String s)
+{
+
+                private static readonly String NBSP_PATTERN = "((nbsp;)(<.*>)?(<\\/p>|<\\/h|<\\/li>|<br))";
+
+int i = 0;
+Pattern p = Pattern.compile(NBSP_PATTERN);
+Matcher matcher = p.matcher(s);
+            while (matcher.find())
+            {
+                ++i;
+            }
+            return i;
+        }
+
+        public int countURLs(String s)
+{
+
+        private static readonly String URL_PATTERN = "((https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$\\/)";
+
+int i = 0;
+Pattern p = Pattern.compile(URL_PATTERN);
+Matcher matcher = p.matcher(s);
+            while (matcher.find())
+            {
+                ++i;
+            }
+            return i;
+        }
+
+        public int countWords(String s)
+{
+
+        private static readonly String WORD_PATTERN = "(\\b\\s?\\w+-?\'?\\w*\\b)";
+
+int i = 0;
+Pattern p = Pattern.compile(WORD_PATTERN);
+Matcher matcher = p.matcher(s);
+            while (matcher.find())
+            {
+                ++i;
+            }
+            return i;
+        }
+
+        public int countSyllables(String s)
+{
+
+        private static readonly String SYLLABLE_PATTERN = "((?i)(?!ed\\b)([aeiouy]{1,3})(?<!e\\b(?<!le\\b)(?<!the|be|he|me)))";
+
+int i = 0;
+Pattern p = Pattern.compile(SYLLABLE_PATTERN);
+Matcher matcher = p.matcher(s);
+            while (matcher.find())
+            {
+                ++i;
+            }
+            return i;
+        }
+
+        public int countSentencesPreHTMLStrip(String s)
+{
+    int i = 0;
+    Pattern p = Pattern.compile(PRE_HTML_STRIP_SENTENCE_PATTERN);
+    Matcher matcher = p.matcher(s);
+    while (matcher.find())
+    {
+        ++i;
+    }
+    return i;
+}
+
+public int countSentencesPostHTMLStrip(String s)
+{
+
+                private static readonly String POST_HTML_STRIP_SENTENCE_PATTERN = "(\\s?([A-Za-z]{2,}|[0-9]+)[\\}\\]\\)\"\']*[.!?;]+[\"\']?)";
+
+int i = 0;
+Pattern p = Pattern.compile(POST_HTML_STRIP_SENTENCE_PATTERN);
+Matcher matcher = p.matcher(s);
+            while (matcher.find())
+            {
+                ++i;
+            }
+            return i;
+        }
+    }
+}
---
+++ ReadabilityChecker/ReadabilityChecker/RCTracker.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadabilityChecker
+{
+    class RCTracker
+    {
+        // counters of different items found in provided text 
+        private int numWords;
+        private int numSentences;
+        private int numSyllables;
+        private int numClosingPTags;
+        private int numIPs;
+        private int numLists;
+        private int numNBSPs;
+
+        // calculated from the counter values
+        private double numWordsPerSentence;
+        private double numSyllablesPerWord;
+
+        // after calculations, used to display readability scores
+        private double FKGrade;
+        private double FKScore;
+
+        // constructor initializes counters
+        public RCTracker()
+        {
+            numWords = 0;
+            numSentences = 0;
+            numSyllables = 0;
+            numClosingPTags = 0;
+            numIPs = 0;
+            numLists = 0;
+            numNBSPs = 0;
+            numWordsPerSentence = 0;
+            numSyllablesPerWord = 0;
+            FKGrade = 0;
+            FKScore = 0;
+        }
+
+        // getters
+        public int getNumWords() { return this.numWords; }
+        public int getNumSentences() { return this.numSentences; }
+        public int getNumSyllables() { return this.numSyllables; }
+        public int getNumClosingPTags() { return this.numClosingPTags; }
+        public int getNumIPs() { return this.numIPs; }
+        public int getNumLists() { return this.numLists; }
+        public int getNumNBSPs() { return this.numNBSPs; }
+        public Double getNumWordsPerSentence() { return numWordsPerSentence; }
+        public Double getNumSyllablesPerWord() { return numSyllablesPerWord; }
+        public Double getFKScore() { return this.FKScore; }
+        public Double getFKGrade() { return this.FKGrade; }
+
+        // setters
+        public void setNumWords(int numWords) { this.numWords = numWords; }
+        public void setNumSentences(int numSentences) { this.numSentences = numSentences; }
+        public void setNumSyllables(int numSyllables) { this.numSyllables = numSyllables; }
+        public void setNumClosingPTags(int numClosingPTags) { this.numClosingPTags = numClosingPTags; }
+        public void setNumIPs(int numIPs) { this.numIPs = numIPs; }
+        public void setNumLists(int numLists) { this.numLists = numLists; }
+        public void setNumNBSPs(int numNBSPs) { this.numNBSPs = numNBSPs; }
+        public void setNumWordsPerSentence(double numWordsPerSentence) { this.numWordsPerSentence = numWordsPerSentence; }
+        public void setNumSyllablesPerWord(double numSyllablesPerWord) { this.numSyllablesPerWord = numSyllablesPerWord; }
+        public void setFKScore(double FKScore) { this.FKScore = FKScore; }
+        public void setFKGrade(double FKGrade) { this.FKGrade = FKGrade; }
+
+
+        public void applyModifiers()
+        {
+            // Don't count things like "1." and "2." at the beginning of lists as words or sentences
+            numWords -= numLists;
+            numSentences -= numLists;
+
+            if (numIPs > 0)
+            {
+                // Don"t count every octet of an IP address as a word
+                numWords -= (numIPs * 4);
+
+                // Count entire IP address as exactly 1 word and 1 syllable
+                numWords += numIPs;
+                numSyllables += numIPs;
+
+                // Since IP addresses have periods, they will artificially boost sentence count by 3
+                // To correct this, substract 3 sentences per IP detected
+                numSentences -= (numIPs * 3);
+            }
+        } // end applyModifiers
+
+        public void calculateNumWordsPerSentence()
+        {
+            if (numSentences > 0)
+            {
+                setNumWordsPerSentence((double)getNumWords() / (double)getNumSentences());
+            }
+        }
+
+        public void calculateNumSyllablesPerWord()
+        {
+            if (numWords > 0)
+            {
+                setNumSyllablesPerWord((double)numSyllables / (double)numWords);
+            }
+        }
+
+        public void calculateFKScore()
+        {
+            // calculate the Flesch-Kincaid Score
+            FKScore = (206.835 - (1.015 * numWordsPerSentence) - (84.6 * numSyllablesPerWord));
+            if (FKScore < 0)
+            {
+                FKScore = 0;  // minimum score = 0
+            }
+            if (FKScore > 100)
+            {
+                FKScore = 100;  // max score = 100
+            }
+        }
+
+        public void calculateFKGrade()
+        {
+            // calculate the Flesch-Kincaid Grade
+            FKGrade = ((0.39 * numWordsPerSentence) + (11.8 * numSyllablesPerWord - 15.59));
+            if (FKGrade < 0)
+            {
+                FKGrade = 0;
+            } //minimum score = 0
+        }
+    }
+}
